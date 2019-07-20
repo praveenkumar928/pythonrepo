5b2057aa51f9a535885ec343d7b06df5a7eb6e38
praveenkumar928 <39269748+praveenkumar928@users.noreply.github.com>
2019-07-19 22:09:30 -0400

    Add files via upload

---
+++ fileread.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+file_path='fileread.txt'
+with open(file_path) as fileobject:
+    filecontent=fileobject.read()
+    print(filecontent)
+
+
+# In[ ]:
+
+
+
+
